83e063aa178085e02d7fa0e3290f1e02bb836d10
saltfog <saltfog@gmail.com>
2018-01-20 15:36:15 -0700

    Final Commit
    Cache Matrix Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Create a matrix that can cache its inverse
+makeCachematrix <- function( mt = matrix() ) {
+  
+  # Set inverse property
+  i <- NULL
+  
+  # Set the matrix function
+  set <- function(matrix) {
+    mt <<- matrix
+    i <<- NULL
+  }
+  
+  # Get the matrix function
+  get <- function() {
+    mt
+  }
+  
+  # Set the inverse of the matrix function
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  # Get the inverse of the matrix function
+  getInverse <- function() {
+    i
+  }
+  
+  # Return a list of our functions
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# Compute the inverse of the special matrix returned by "makeCachematrix"
+# above. If the inverse has already been calculated (and the matrix has not
+# changed), then the "cachesolve" should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Return a matrix that is the inverse of 'x'
+  mt <- x$getInverse()
+  
+  # Just return the inverse if its already set
+  if( !is.null(mt) ) {
+    return(mt)
+  }
+  
+  # Get the matrix from our obj
+  data <- x$get()
+  
+  # Calculate the inverse using matrix mtultiplication
+  mt <- solve(data) %*% data
+  
+  # Set the inverse to the obj
+  x$setInverse(mt)
+  
+  # Return the matrix
+  mt
 }
